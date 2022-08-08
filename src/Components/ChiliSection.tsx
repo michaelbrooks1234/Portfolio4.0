b68bfaf8d23e2a746fb4e6219112e2b911362d4b
Michael <michaelbrooks6897@gmail.com>
2022-08-08 13:31:37 -0400

    Added some styling

--- src/Components/ChiliSection.tsx
+++ src/Components/ChiliSection.tsx
@@ -13,13 +13,13 @@ class ChiliSection extends React.Component {
                 <h1 className="JobTimePeriod">
                    From Dec 13 2021 - Present. 
                 </h1>
-                <div>
+                <div className="JobDescription">
                     <ul>
-                        <li>Job Description</li>
-                        <li>Job Description</li>
-                        <li>Job Description</li>
-                        <li>Job Description</li>
-                        <li>Job Description</li>
+                        <li>Levereged web developer knowledge to lead and support clients to thrive while integrating our product.</li>
+                        <li>Constructed reliable proof of concepts for clients by writing clean, readable, and maintainable code.</li>
+                        <li>Managed customers data regarding our product through Azure by utilizing Azure's blob storage and file shares.</li>
+                        <li>Showcased correct usage of our API by creating example HTTP requests. Effectively troubleshooted API issues reported by clients.</li>
+                        <li>Assisted in increasing customer satisfaction from less than 80% to 97.5%.</li>
                     </ul>
                 </div>
             </div>
