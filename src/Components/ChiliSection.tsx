5434526bf08eac0ba7169bc3ff410a7bd8f4dd2a
Michael <michaelbrooks6897@gmail.com>
2022-08-23 19:49:52 -0400

    Changed content

--- src/Components/ChiliSection.tsx
+++ src/Components/ChiliSection.tsx
@@ -15,11 +15,10 @@ class ChiliSection extends React.Component {
                 </h1>
                 <div className="JobDescription">
                     <ul>
-                        <li>Levereged web developer knowledge to lead and support clients to thrive while integrating our product.</li>
-                        <li>Constructed reliable proof of concepts for clients by writing clean, readable, and maintainable code.</li>
-                        <li>Managed customers data regarding our product through Azure by utilizing Azure's blob storage and file shares.</li>
-                        <li>Showcased correct usage of our API by creating example HTTP requests. Effectively troubleshooted API issues reported by clients.</li>
-                        <li>Assisted in increasing customer satisfaction from less than 80% to 97.5%.</li>
+                        <li>Leveraged Node.js and the vast variety of libraries in npm to create high functionality scripts that could be re-used efficiently.</li>
+                        <li>Fully developed web app integrations of the product with React and TypeScript packaged into an electron app for easy distribution.</li>
+                        <li>Managed customers data regarding our product through Azure by utilizing Azure's blob storage and file shares..</li>
+                        <li>showcased correct usage of our API by creating example REST calls utilizing node.js and fetch.</li>
                     </ul>
                 </div>
             </div>
